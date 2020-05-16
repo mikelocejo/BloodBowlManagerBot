b3386475882d36d06c8760e1f5b07550d2f63954
latra <personadelmonton@gmail.com>
2020-05-16 20:41:07 +0200

    Añadido script de configuración de tablas sql

---
+++ setupDB.py
@@ -0,0 +1,20 @@
+# Setup a empty DB with the needed rows
+from dotenv import load_dotenv
+import sqlite3, os
+
+QUERYS = [  'CREATE TABLE "tournaments" ' +
+                '("id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"idDiscord"  TEXT(30) NOT NULL,"league"  TEXT(100),"tournament"  TEXT(100),"goblinValue"  TEXT(100));'
+        ]
+
+
+
+
+
+load_dotenv()
+connect = sqlite3.connect(os.getenv('SQLITE_CONNECTION'))
+cursor = connect.cursor()
+for query in QUERYS: 
+    cursor.execute(query)
+
+connect.commit()
+cursor.close()
