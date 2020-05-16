6a64923381a60eb8a23962ff5e9f17a2009a0341
mikel-ocejo <mocejo004@gmail.com>
2020-05-16 19:53:25 +0200

    Update bloodBowl.py
    Utilizar diccionario para eliminar spagetti code

--- bloodBowl.py
+++ bloodBowl.py
@@ -1,53 +1,6 @@
 def Get_Race(race_id):
-    print(race_id)
-    if race_id == 0:
-        return ''
-    elif race_id == 1:
-        return ':bbhuman:'
-    elif race_id == 2:
-        return ':bbdwarf:'
-    elif race_id == 3:
-        return ':bbskaven:'
-    elif race_id == 4:
-        return ':bborc:'
-    elif race_id == 5:
-        return ''
-    elif race_id == 6:
-        return ''
-    elif race_id == 7:
-        return ':bbwoodElf:'
-    elif race_id == 8:
-        return ''
-    elif race_id == 9:
-        return ':bbdarkElfs:'
-    elif race_id == 10:
-        return ''
-    elif race_id == 11:
-        return ''
-    elif race_id == 12:
-        return ':bbnorses:'
-    elif race_id == 13:
-        return ':bbamazon:'
-    elif race_id == 14:
-        return ''
-    elif race_id == 15:
-        return ':bbhighElf:'
-    elif race_id == 16:
-        return ':bbkhemri:'
-    elif race_id == 17:
-        return ':bbnecromantic:'
-    elif race_id == 18:
-        return ''
-    elif race_id == 19:
-        return ''
-    elif race_id == 20:
-        return ':bbvampire:'
-    elif race_id == 21:
-        return ':bbchaosDwarf:'
-    elif race_id == 22:
-        return ''
-    elif race_id == 23:
-        return ''
-    elif race_id == 24:
-        return ':bbbretonnia:'
+    races_dict = {1:':bbhuman:',2:':bbdwarf:',3:':bbskaven:',4:':bborc:',7:':bbwoodElf:',9:':bbdarkElfs:',12:':bbnorses:',13:':bbamazon:',15:':bbhighElf:',16:':bbkhemri:',17:':bbnecromantic:',20:':bbvampire:',21:':bbchaosDwarf:',24:':bbbretonnia:'}
+    race = races_dict.get(race_id)
+    if race:
+        return race
     return 'Unknown'
